dfbfb8bf82d8c92b083df56917384e696600c279
unknown <mukulsomukesh@gmail.com>
2023-02-16 20:09:20 +0530

    created api for update & delete file

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -36,4 +36,42 @@ const getSingleFile = (req, res) => {
     });
 };
 
-module.exports = {getFile , uploadFile, getSingleFile}
+
+const updateFile = async (req,res) => {
+  const {name,fileType,isProtected,password, fileData} = req.body;
+
+  const data = await File.findById(req.params.id);
+
+  if(data) {
+    data.name=name,
+    data.fileType=fileType,
+    data.isProtected=isProtected,
+    data.password=password,
+    data.fileData=fileData
+    // data.user=req.user._id
+
+      const updateData = await data.save();
+// console.log(name,fileType,isProtected,password, fileData,user_id)    
+      res.json({message:"File Updated.", data:data})
+  } else {
+      res.status(404);
+      throw new Error("File not found")
+  }
+}
+
+
+const deleteFile = async(req,res) => {
+  const data = await File.findById(req.params.id);
+
+  if(data){
+      await data.remove();
+      res.json({message:"File Removed."})
+  } else {
+      res.status(404);
+      throw new Error("File not found")
+  }
+}
+
+
+
+module.exports = {getFile , uploadFile, getSingleFile, deleteFile, updateFile}
